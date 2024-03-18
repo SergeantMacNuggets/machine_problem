c47084f571a99003913816302ecc8f04a2d58541
Cedrick Senados <cd.cabz12@gmail.com>
2024-03-18 18:42:26 +0800

    Not finished yet

--- Group4/problem4.c
+++ Group4/problem4.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Problem #4:
-Given an integer array nums and an integer k, return the number of good subarrays of
-nums.
-A good array is an array where the number of different integers in that array is exactly k.
-For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
-A subarray is a contiguous part of an array.
-*/
-
-/*Example 1:
-    Input: nums = [1,2,1,2,3], k = 2
-    Output: 7*/
 int differentInt(int *nums,int k) {
     int diff=1,j;
     for(int i=1;i<k;i++) {
@@ -25,48 +14,52 @@ int differentInt(int *nums,int k) {
     return diff;
 }
 
-int subArrays(int* ,int, int,int,int );
+int subArrays(int* ,int, int);
 
 int main() {
-    int nums[] = {1,2,1,2,3};
+    int nums[]={1,2,1,2,3};
+    int k=2;
     int numsSize=sizeof(nums)/sizeof(nums[0]);
-    int k = 2;
-    int perm=k;
     int output = 0;
-    int result = subArrays(nums,numsSize,k,perm,output);
+    int perm = k;
+    int result = subArrays(nums,numsSize,k);
     printf("Output is %d\n",result);
 
     return 0;
 }
-int subArrays(int *nums,int numsSize,int k,int perm,int output) {
-    int i=0,x=k,y,diff;
+int subArrays(int *nums,int numsSize,int k) {
+    int i=0,perm=k,output=0,x=perm,diff;
     int *temp_arr=(int *)malloc(numsSize*sizeof(int));
     if(k==1) {
         output=numsSize;
         return output;
     }
+    while(x<=numsSize+1) {
     //Read array from index[0] to index[k]
-    while(i<=numsSize && k < numsSize+1) {
+        while(i<=numsSize && perm < numsSize+1) {
         //Check if index[k] has reach the end of array
-        if(x==numsSize+1) {
+            if(x==numsSize+1) {
             //If true check if k is equal to numsSize
-            if(k==numsSize) {
-                return output;
-            }
+                if(perm==numsSize) {
+                    printf("x is %d\n",x);
+                    return output;
+                }
             //If false perform a recursion
-            subArrays(nums,numsSize,k+1,perm,output);
-            break;
+                break;
+            }
+            for(int j=i,y=0;j<x && x<=numsSize;y++,j++) {
+                temp_arr[y]=nums[j];
+                printf("%d ",temp_arr[y]);
+            }
+            printf("*x=%d\n",x);
+            diff = differentInt(temp_arr,perm);
+            if(diff==k) {
+                output+=1;
+            }
+            x++;
+            i++;
         }
-        for(int j=i,y=0;j<x && x<=numsSize;y++,j++) {
-            temp_arr[y]=nums[j];
-            printf("%d ",temp_arr[y]);
-        }
-        printf("\n",k);
-        diff = differentInt(temp_arr,k);
-        if(diff==perm) {
-            output+=1;
-        }
-        x++;
-        i++;
+        perm+=1;
+        printf("perm is %d\n",perm);
     }
 }
