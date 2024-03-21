176a04ab4980ddd9fe37fca0931c4b0f9662376c
Cedrick Senados <cd.cabz12@gmail.com>
2024-03-21 14:08:17 +0800

    Added contraints

--- Group4/problem4.c
+++ Group4/problem4.c
@@ -21,8 +21,38 @@ int differentInt(int *nums,int k) {
     return diff;
 }
 
+int indexContraints(int n,int numsSize,int i) {
+    if(i>=0) {
+        while(true) {
+            printf("nums[%d] = ",i);
+            scanf("%d",&n);
+            if(1>n||n>numsSize)
+                printf("Nums[i] must be > or = to 1 and < or = to size of the array\n");
+            else
+                break;
+        }
+    }
+    else if(i<0) {
+        while(true) {
+            printf("How many different integers to be scanned? k = ");
+            scanf("%d",&n);
+            if(1 > n || n > numsSize)
+                printf("k must be > or = to 1 and < or = to size of the array\n");
+            else 
+                break;
+        }
+    }
+    return n;
+}
+
+bool numsConstraint(int numsSize) {
+    if(1>numsSize || numsSize>20000)
+        return true;
+    return false;
+}
+
 int __rec__fuc(int* ,int, int, int, int);
-void displayArr(int *,int);
+void displaySubArr(int *,int,int);
 
 int subArrays(int *nums,int numsSize,int k) {
     int perm=k;
@@ -38,15 +68,20 @@ int main() {
     printf("How many integer would you like to add? ");
     scanf("%d",&u1.numsSize);
     u1.nums=(int *)malloc(u1.numsSize*sizeof(int));
+    if(numsConstraint(u1.numsSize)==true) {
+        printf("Size of the array must be > or == to 1 and < or = 2 x 10^4\n");
+        main();
+    }
     printf("Input %d numbers:\n",u1.numsSize);
     for(int i=0;i<u1.numsSize;i++) {
-        printf("nums[%d] = ",i);
-        scanf("%d",&u1.nums[i]);
+        u1.nums[i]=indexContraints(u1.nums,u1.numsSize,i);
     }
-    printf("How many different integers to be scanned? ");
-    scanf("%d",&u1.k);
+    u1.k=indexContraints(u1.k,u1.numsSize,-1);
+    system("cls");
+    displaySubArr(u1.nums,u1.numsSize,-1);
+    printf("Sub Arrays:\n");
     int result = subArrays(u1.nums,u1.numsSize,u1.k);
-    printf("Output is %d\n",result);
+    printf("\nOutput is %d\n",result);
 
     return 0;
 }
@@ -70,19 +105,44 @@ int __rec__fuc(int *nums,int numsSize,int k,int perm,int output) {
         }
         diff = differentInt(temp_arr,perm);
         if(diff==k) {
-            displayArr(temp_arr,perm);
             output+=1;
+            displaySubArr(temp_arr,perm,output);
         }
         x++;
         i++;
     }
 }
-
-void displayArr(int *arr,int perm) {
-    printf("| ");
-    for(int i=0;i<perm;i++) {
-        printf("%d | ",arr[i]);
+void displaySubArr(int *arr,int perm,int output) { 
+    if(output < 0) {
+        printf("Arrays:\n");
+        for(int u=0;u<perm;u++) {
+            printf(" ___");
+        }
+        printf("\n");
+        printf("| ");
+        for(int i=0;i<perm;i++) {
+            printf("%d | ",arr[i]);
+        }
+        printf("\n");
+        for(int u=0;u<perm;u++) {
+            printf("*---");
+        }
+        printf("*\n\n");
     }
-    printf("\n");
+    else {
+        for(int u=0;u<perm;u++) {
+            printf(" ___");
+        }
+        printf("\n");
+        printf("| ");
+        for(int i=0;i<perm;i++) {
+            printf("%d | ",arr[i]);
+        }
+        printf("= subArray%d\n",output);
+        for(int u=0;u<perm;u++) {
+            printf("*---");
+        }
+        printf("*\n");
+    }
 }
 
